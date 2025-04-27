9dc223ce4eed168a656e3304761e80935aca6b95
Jason Lang <raptorify@gmail.com>
2025-04-27 23:44:02 +0200

    Add escape menu and some default ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
+import './reset.css'
+import './ui/fonts/gameovercre.css'
+import './ui/buttons/primary.css'
 import Game from "./components/game/Game.tsx";
-import EscapeMenu from "./components/game/EscapeMenu.tsx";
+import EscapeMenu from "./components/game/EscapeMenu/EscapeMenu.tsx";
 
 function App() {
 
-  return (
-    <>
-        <EscapeMenu />
-        <Game></Game>
-    </>
-  )
+    return (
+        <>
+            <EscapeMenu/>
+            <Game></Game>
+        </>
+    )
 }
 
 export default App
