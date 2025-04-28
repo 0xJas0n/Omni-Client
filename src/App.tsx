53154fade940b0c96b60ebe631897c9a9cf2da6d
Jason Lang <raptorify@gmail.com>
2025-04-28 17:14:18 +0200

    Create ui components directory

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './reset.css'
 import './ui/fonts/gameovercre.css'
 import './ui/buttons/primary.css'
 import Game from "./components/game/Game.tsx";
-import EscapeMenu from "./components/game/EscapeMenu/EscapeMenu.tsx";
+import EscapeMenu from "./components/ui/EscapeMenu/EscapeMenu.tsx";
 
 function App() {
 
